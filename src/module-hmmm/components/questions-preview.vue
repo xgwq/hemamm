9296bc620b85d33c45ec3cbd8781ce77db8cc344
yys <1186591377@qq.com>
2021-02-21 20:15:33 +0800

    组题列表浏览功能

--- src/module-hmmm/components/questions-preview.vue
+++ src/module-hmmm/components/questions-preview.vue
@@ -1,9 +1,63 @@
 <template>
-  <div class='container'>试题预览对话框</div>
+  <div class='container'>
+    <div class="app-container">
+      <el-dialog
+        title="题目预览"
+        :visible.sync="dialogVisible"
+        :before-close="handleClose">
+          <el-row :gutter="20">
+            <el-col :span="6">
+              <span>{{`【${this.previewDetailData.questionType === "1"? "单选" : this.previewDetailData.questionType === "2" ? "多选" : "简答"}】`}}</span>
+            </el-col>
+          </el-row>
+          <span slot="footer" class="dialog-footer">
+           <el-button type="primary" @click="dialogVisible = false">关闭</el-button>
+          </span>
+      </el-dialog>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+import { previewDetail } from '@/api/randomsapi/randomsapi'
+export default {
+  name: 'preview',
+  data () {
+    return {
+      previewDetailData: {},
+      dialogVisible: false// 开关弹窗，标识
+    }
+  },
+  props: {
+    questionsObj: Object, // 试题预览id
+    dialogB: Boolean// 点击预览-传入true
+  },
+  watch: {
+    async questionsObj (newval) { // 监听试题id
+      // console.log(newval)// 之后调用试题详情接口
+      const respreviewDetail = await previewDetail(newval)
+      console.log(respreviewDetail)
+      this.previewDetailData = respreviewDetail.data
+    },
+    dialogB (newval) { // 监听传入布尔值
+      // console.log(newval, oldval)
+      this.dialogVisible = newval
+    },
+    dialogVisible (newval) { /// 监听 开关弹窗，标识，将false传出去
+      this.$emit('dialogVisible', newval)
+    }
+  },
+  methods: {
+    handleClose (done) { // 关闭x
+      this.$confirm('确认关闭？')
+        .then(_ => {
+          this.dialogVisible = false
+          done()
+        })
+        .catch(_ => {})
+    }
+  }
+}
 </script>
 
-<style scoped lang='less'></style>
+<style rel="stylesheet/scss" lang="scss" scoped></style>
--- src/module-hmmm/pages/questions-randoms.vue
+++ src/module-hmmm/pages/questions-randoms.vue
@@ -1,11 +1,203 @@
 <template>
-  <div class='container'>精选题库123
-
+  <div class='container'>
+    <div class="app-container">
+      <el-card class="box-card">
+        <el-form ref="form" :model="form" label-width="80px" :inline="true">
+          <el-row>
+            <el-col :span="20">
+              <el-form-item label="关键字">
+                <el-input v-model="form.keyword"></el-input>
+              </el-form-item>
+            </el-col>
+            <el-col :span="4" class="button">
+              <el-form-item>
+                <el-button @click="removeBtn">清除</el-button>
+                <el-button type="primary" @click="searchBtn">搜索</el-button>
+              </el-form-item>
+            </el-col>
+          </el-row>
+        </el-form>
+        <!-- 弹窗 -->
+        <el-alert
+          :title="alertText"
+          type="info"
+          :closable="false"
+          show-icon>
+        </el-alert>
+        <!-- 列表 -->
+        <el-table
+          :data="tableData"
+          style="width: 100%; margin-top:20px;">
+          <el-table-column
+            prop="id"
+            label="编号"
+            width="190">
+          </el-table-column>
+          <el-table-column
+            prop="questionType"
+            label="题型"
+            width="110">
+            <template v-slot="scope">
+              <span>{{ scope.row.questionType === "1" ? "单选" : scope.row.questionType === "2" ?"多选" : "简答"}}</span>
+            </template>
+          </el-table-column>
+          <el-table-column
+            prop="questionIDs"
+            label="题目编号"
+            width="190">
+            <template v-slot="scope">
+              <div
+              v-for="(obj,index) in scope.row.questionIDs"
+              :key="index">
+                <el-button type="text" @click="previewBtn(obj)">{{ obj.number }}</el-button>
+              </div>
+            </template>
+          </el-table-column>
+          <el-table-column
+            prop="addTime"
+            label="录入时间"
+            width="180">
+          </el-table-column>
+          <el-table-column
+            prop="totalSeconds"
+            label="答题时间"
+            width="150">
+          </el-table-column>
+          <el-table-column
+            prop="accuracyRate"
+            label="真确率（%）"
+            width="150">
+          </el-table-column>
+          <el-table-column
+            prop="userName"
+            label="录入人"
+            width="150">
+          </el-table-column>
+          <el-table-column
+            label="操作"
+            width="138">
+            <template v-slot="scope">
+              <el-button size="small" type="danger" icon="el-icon-delete" circle @click="delfandomBtn(scope.row)"></el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+        <!-- 分页器 -->
+        <div class="pagination" >
+          <div class="page">
+            <el-pagination
+              style="margin-top:20px;"
+              background
+              @current-change="handleCurrentChange"
+              :current-page="page"
+              @size-change="handleSizeChange"
+              :page-sizes="[20, 30, 40, 50]"
+              :page-size="pagesize"
+              layout="prev, pager, next, sizes, jumper"
+              :total="total">
+            </el-pagination>
+          </div>
+        </div>
+        <!-- 预览组件使用 -->
+        <Rpreview :questionsObj="questionsObj" :dialogB="dialogVisible" @dialogVisible="prevBtn"></Rpreview>
+      </el-card>
+    </div>
   </div>
 </template>
 
 <script>
-export default {}
+import { randomsList, delRandom } from '@/api/randomsapi/randomsapi.js'
+import Rpreview from './../components/questions-preview'
+export default {
+  name: 'randoms',
+  data () {
+    return {
+      form: {
+        keyword: ''
+      },
+      questionsObj: {},
+      dialogVisible: false,
+      page: 1, // 第1页
+      pagesize: 20, // 分页器-每页10条
+      total: 0,
+      alertText: '',
+      tableData: []// 列表数据
+    }
+  },
+  methods: {
+    handleSizeChange (val) { // 分页器
+      // console.log(val)
+      this.pagesize = val
+      this.page = 1
+      this.getrandomsList()
+    },
+    handleCurrentChange (val) { // 当前页数
+      this.page = val
+      this.getrandomsList()// 获取组题列表
+    },
+    removeBtn () { // 清除
+      // console.log(1)
+      this.form.keyword = ''
+    },
+    searchBtn () { // 搜索
+      this.page = 1
+      this.pagesize = 20
+      this.getrandomsList()// 获取组题列表
+      this.form.keyword = ''
+    },
+    delfandomBtn (obj) { // 删除
+      // console.log(obj)
+      this.$confirm('此操作将永久删除, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(async () => {
+        await delRandom(obj)
+        this.$message({
+          type: 'success',
+          message: '删除成功!'
+        })
+        this.getrandomsList()// 获取组题列表
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        })
+      })
+    },
+    previewBtn (obj) { // 预览
+      // console.log(obj)
+      this.questionsObj = obj
+      // console.log(this.questionsID)
+      this.dialogVisible = true
+    },
+    prevBtn (val) { // 自定义事件
+      // console.log(val)
+      this.dialogVisible = val
+    },
+    async getrandomsList () { // 获取组题列表
+      const resrandomsparams = await randomsList({
+        page: this.page,
+        pagesize: this.pagesize,
+        ...this.form
+      })
+      // console.log(resrandomsparams)
+      this.tableData = resrandomsparams.data.items// 表格数据
+      this.total = resrandomsparams.data.counts// 数据总数
+      this.alertText = `总条数为${this.total}条`
+    }
+  },
+  created () {
+    this.getrandomsList()// 获取组题列表
+  },
+  components: {
+    Rpreview
+  }
+}
 </script>
 
-<style scoped lang='less'></style>
+<style rel="stylesheet/scss" lang="scss" scoped>
+.button , .pagination{
+  display: flex;
+  flex-direction:row-reverse;
+}
+</style>
